5e30f0420358e077422123424bcca3d5dd8ea68f
eisnerh <elopeza@ucatolica.ac.cr>
2018-02-26 00:04:11 -0600

    modelos completos

--- Agenda/models.py
+++ Agenda/models.py
@@ -7,22 +7,22 @@ from django.db import models
 
 class Cliente(models.Model):
     idCliente = models.AutoField(primary_key=True)
-    Nombre_cliente = models.TextField()
-    Apellido_cliente = models.TextField()
-    Telefono_cliente = models.TextField()
-    Correo_electronico = models.TextField()
+    Nombre_cliente = models.CharField(max_length=45)
+    Apellido_cliente = models.CharField(max_length=45)
+    Telefono_cliente = models.CharField(max_length=45)
+    Correo_electronico = models.CharField(max_length=45)
     Otros_datos_cliente = models.TextField()
 
 
 class ClasificacionEvento(models.Model):
     idClasificacion_evento = models.AutoField(primary_key=True)
-    Clasificacion_evento = models.TextField()
+    Clasificacion_evento = models.CharField(max_length=45)
 
 
 class Evento(models.Model):
     idEvento = models.AutoField(primary_key=True)
-    NombreEvento = models.TextField()
-    CodigoEvento = models.TextField()
+    NombreEvento = models.CharField(max_length=45)
+    CodigoEvento = models.CharField(max_length=45)
 
 
 class DetalleEvento(models.Model):
@@ -35,19 +35,19 @@ class DetalleEvento(models.Model):
 
 class EventoCliente(models.Model):
     idEventoCliente = models.AutoField(primary_key=True)
-    ClienteIdCliente = models.IntegerField()
+    ClienteIdCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     EventoIdEvento = models.ForeignKey(Evento, on_delete=models.CASCADE)
 
 
 class Material(models.Model):
     idMaterial = models.AutoField(primary_key=True)
-    CodigoMaterial = models.TextField()
-    nombreMaterial = models.TextField()
+    CodigoMaterial = models.CharField(max_length=45)
+    nombreMaterial = models.CharField(max_length=45)
     CantidadIdeal = models.IntegerField()
     CantidadInicial = models.IntegerField()
 
 
 class EventoMaterial(models.Model):
     idEventoMaterial = models.AutoField(primary_key=True)
-    EventoIdEvento = models.IntegerField()
+    EventoIdEvento = models.ForeignKey(Evento, on_delete=models.CASCADE)
     MaterialIdMaterial = models.ForeignKey(Material, on_delete=models.CASCADE)
--- Agenda/urls.py
+++ Agenda/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
 urlpatterns = [
-    path('hello/', ),
-]
+
+        path(r'', 'Agenda.urls'),
+    ]
